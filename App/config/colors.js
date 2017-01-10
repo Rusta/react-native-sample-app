e685b1bde1b642cb4a61b450ba60054e7ecdd3a1
Rusta <rusta@rusta.co.uk>
2017-01-10 09:29:53 +0000

    add custom nav colours

---
+++ App/config/colors.js
@@ -0,0 +1,4 @@
+export const main = '#8B80F9';
+
+export const text = 'black';
+export const subText = '#83858c';
--- App/index.js
+++ App/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
 import Router from './Router';
 
 export default class App extends Component {
+  componentWillMount() {
+    StatusBar.setBarStyle('light-content', true);
+  }
   render() {
     return (
       <Router />
